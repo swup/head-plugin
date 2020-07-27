b96eb4cde398d115d0e0f942f049b7c8b39dc862
Philipp Daun <post@philippdaun.net>
2020-07-27 13:09:55 +0200

    Update html language attribute

--- src/index.js
+++ src/index.js
@@ -25,10 +25,12 @@ export default class HeadPlugin extends Plugin {
 
 	mount() {
 		this.swup.on('contentReplaced', this.getHeadAndReplace);
+		this.swup.on('contentReplaced', this.updateHtmlLangAttribute);
 	}
 
 	unmount() {
 		this.swup.off('contentReplaced', this.getHeadAndReplace);
+		this.swup.off('contentReplaced', this.updateHtmlLangAttribute);
 	}
 
 	getHeadAndReplace = () => {
@@ -133,5 +135,20 @@ export default class HeadPlugin extends Plugin {
 			return item.matches(persistTags);
 		}
 		return Boolean(persistTags);
+	};
+
+	updateHtmlLangAttribute = () => {
+		const html = document.documentElement;
+
+		const newPage = new DOMParser().parseFromString(
+			this.swup.cache.getCurrentPage().originalContent,
+			'text/html'
+		);
+		const newLang = newPage.documentElement.lang;
+
+		if (html.lang !== newLang) {
+			this.swup.log(`Updated lang attribute: ${html.lang} > ${newLang}`);
+			html.lang = newLang;
+		}
 	};
 }
