ca8af6acd1776cd8bc7a5df32eedfd09f47326b5
Georgy Marchuk <gmarcuk@gmail.com>
2021-03-13 20:38:57 +0100

    build merged version

--- dist/SwupHeadPlugin.js
+++ dist/SwupHeadPlugin.js
@@ -252,6 +252,18 @@ var HeadPlugin = function (_Plugin) {
 			return Boolean(persistTags);
 		};
 
+		_this.updateHtmlLangAttribute = function () {
+			var html = document.documentElement;
+
+			var newPage = new DOMParser().parseFromString(_this.swup.cache.getCurrentPage().originalContent, 'text/html');
+			var newLang = newPage.documentElement.lang;
+
+			if (html.lang !== newLang) {
+				_this.swup.log('Updated lang attribute: ' + html.lang + ' > ' + newLang);
+				html.lang = newLang;
+			}
+		};
+
 		_this.options = _extends({}, _this.defaultOptions, options);
 
 		// options.persistAssets is a shortcut to:
@@ -266,11 +278,13 @@ var HeadPlugin = function (_Plugin) {
 		key: 'mount',
 		value: function mount() {
 			this.swup.on('contentReplaced', this.getHeadAndReplace);
+			this.swup.on('contentReplaced', this.updateHtmlLangAttribute);
 		}
 	}, {
 		key: 'unmount',
 		value: function unmount() {
 			this.swup.off('contentReplaced', this.getHeadAndReplace);
+			this.swup.off('contentReplaced', this.updateHtmlLangAttribute);
 		}
 	}]);
 
