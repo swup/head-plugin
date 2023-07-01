ce2ea329af3f4782ec4721b2f0f2843064a4de79
Philipp Daun <post@philippdaun.net>
2023-07-01 11:42:04 +0200

    Update for swup v4

--- src/index.js
+++ src/index.js
@@ -4,38 +4,35 @@ import mergeHeadContents from './mergeHeadContents.js';
 import updateLangAttribute from './updateLangAttribute.js';
 import waitForAssets from './waitForAssets.js';
 
-export default class HeadPlugin extends Plugin {
-	name = 'HeadPlugin';
+export default class SwupHeadPlugin extends Plugin {
+	name = 'SwupHeadPlugin';
+
+	defaults = {
+		persistTags: false,
+		persistAssets: false,
+		awaitAssets: false,
+		timeout: 3000
+	};
 
 	assetLoadPromises = [];
 
 	constructor(options = {}) {
 		super();
 
-		this.options = {
-			persistTags: false,
-			persistAssets: false,
-			awaitAssets: false,
-			timeout: 3000,
-			...options
-		};
-	}
+		this.options = { ...this.defaults, ...options };
 
-	mount() {
-		this.validateOptions();
-
-		// Replace head contents right before content itself
-		this.swup.on('willReplaceContent', this.updateHead);
-
-		// Overwrite swup's replaceContent to let us defer until all assets are loaded
-		if (this.options.awaitAssets) {
-			this.originalSwupReplaceContent = this.swup.replaceContent.bind(this.swup);
-			this.swup.replaceContent = this.replaceContentAfterAssetsLoaded.bind(this);
+		// persistAssets is a shortcut for: persistTags with a default asset selector for scripts & styles
+		if (this.options.persistAssets && !this.options.persistTags) {
+			this.options.persistTags = 'link[rel=stylesheet], script[src], style';
 		}
 	}
 
+	mount() {
+		this.swup.hooks.before('replaceContent', this.updateHead);
+	}
+
 	unmount() {
-		this.swup.off('willReplaceContent', this.updateHead);
+		this.swup.hooks.off('replaceContent', this.updateHead);
 
 		if (this.originalSwupReplaceContent) {
 			this.swup.replaceContent = this.originalSwupReplaceContent;
@@ -43,55 +40,25 @@ export default class HeadPlugin extends Plugin {
 		}
 	}
 
-	validateOptions() {
-		// options.persistAssets is a shortcut for:
-		// options.persistTags with a default asset selector for scripts & styles
-		if (this.options.persistAssets && !this.options.persistTags) {
-			this.options.persistTags = 'link[rel=stylesheet], script[src], style';
-		}
-
-		// Make sure the swup version in use supports hooking into `replaceContent`
-		if (this.options.awaitAssets && !this.swup.replaceContent) {
-			this.options.awaitAssets = false;
-			console.error('[Swup Head Plugin] Installed version of swup doesn\'t support awaitAssets option');
-		}
-	}
-
-	updateHead = () => {
-		const newPageHtml = this.swup.cache.getCurrentPage().originalContent;
-		let newDocument = new DOMParser().parseFromString(newPageHtml, 'text/html');
+	updateHead = async (context, { page: { html } }) => {
+		const newDocument = new DOMParser().parseFromString(html, 'text/html');
 
 		const { removed, added } = mergeHeadContents(document.head, newDocument.head, { shouldPersist: this.isPersistentTag });
-		const lang = updateLangAttribute(document.documentElement, newDocument.documentElement);
-
 		this.swup.log(`Removed ${removed.length} / added ${added.length} tags in head`);
+
+		const lang = updateLangAttribute(document.documentElement, newDocument.documentElement);
 		if (lang) {
 			this.swup.log(`Updated lang attribute: ${lang}`);
 		}
 
 		if (this.options.awaitAssets) {
-			this.assetLoadPromises = waitForAssets(added, this.options.timeout);
-		} else {
-			this.assetLoadPromises = [];
+			const assetLoadPromises = waitForAssets(added, this.options.timeout);
+			if (this.assetLoadPromises.length) {
+				this.swup.log(`Waiting for ${this.assetLoadPromises.length} assets to load`);
+				await Promise.all(assetLoadPromises);
+			}
 		}
-
-		newDocument.documentElement.innerHTML = '';
-		newDocument = null;
 	};
-
-	replaceContentAfterAssetsLoaded(...originalArgs) {
-		if (this.assetLoadPromises.length) {
-			this.swup.log(`Waiting for ${this.assetLoadPromises.length} assets to load`);
-			return new Promise((resolve) => {
-				Promise.all(this.assetLoadPromises).then(() => {
-					this.assetLoadPromises = [];
-					this.originalSwupReplaceContent(...originalArgs).then(resolve);
-				});
-			});
-		} else {
-			return this.originalSwupReplaceContent(...originalArgs);
-		}
-	}
 
 	isPersistentTag = (el) => {
 		const { persistTags } = this.options;
