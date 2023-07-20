2a3fdc78177e9b376f99a8a1e95eb30c70592227
Philipp Daun <post@philippdaun.net>
2023-07-20 09:35:06 +0200

    Create script loader

---
+++ src/loadScript.js
@@ -0,0 +1,29 @@
+export default function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    const parent = document.head || document.body || document.documentElement;
+
+    // Avoid loading script repeatedly
+    if (parent.querySelector(`script[src*="${url}"]`)) {
+      resolve(url);
+      return;
+    }
+
+    const script = document.createElement('script');
+    const loadend = () => {
+      script.onerror = null;
+      script.onload = null;
+    };
+
+    script.onerror = () => {
+      loadend();
+      reject(new Error(`Failed to load script: ${url}`));
+    };
+    script.onload = () => {
+      loadend();
+      resolve(url);
+    };
+    script.async = true;
+    script.src = url;
+    parent.appendChild(script);
+  }));
+}
