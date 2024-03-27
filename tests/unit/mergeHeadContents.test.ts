8b09d78a1bf2de87244f9dd9232e1bf45352a99f
Philipp Daun <post@philippdaun.net>
2024-03-27 12:47:33 +0100

    Remove comments and unused imports

--- tests/unit/mergeHeadContents.test.ts
+++ tests/unit/mergeHeadContents.test.ts
@@ -1,5 +1,3 @@
-// FILEPATH: /Users/philipp/Projects/@swup/head-plugin/tests/unit/mergeHeadContents.test.ts
-
 import { describe, expect, it } from 'vitest';
 import mergeHeadContents from '../../src/mergeHeadContents.js';
 
--- tests/unit/waitForAssets.test.ts
+++ tests/unit/waitForAssets.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, beforeEach, afterEach } from 'vitest';
+import { describe, expect, it, beforeEach } from 'vitest';
 
 import waitForAssets, { waitForStylesheet } from '../../src/waitForAssets.js';
 
