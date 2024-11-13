270d284e1b49c06c4de0ad51c61e4ff7cc27ccaf
Benjamin Robinet <benjamin@benrbnt.com>
2024-11-13 19:11:41 +0100

    fix: make sure to stop waiting for assets to be loaded if timeout elapsed

--- src/waitForAssets.ts
+++ src/waitForAssets.ts
@@ -13,18 +13,23 @@ export function waitForStylesheet(
 	element: HTMLLinkElement,
 	timeoutMs: number = 0
 ): Promise<HTMLLinkElement> {
+	let timeoutId: ReturnType<typeof setTimeout>;
+
 	const whenLoaded = (cb: () => void) => {
 		if (element.sheet) {
 			cb();
 		} else {
-			setTimeout(() => whenLoaded(cb), 10);
+			timeoutId = setTimeout(() => whenLoaded(cb), 10);
 		}
 	};
 
 	return new Promise((resolve) => {
 		whenLoaded(() => resolve(element));
 		if (timeoutMs > 0) {
-			setTimeout(() => resolve(element), timeoutMs);
+			setTimeout(() => {
+				if (timeoutId) clearTimeout(timeoutId);
+				resolve(element);
+			}, timeoutMs);
 		}
 	});
 }
